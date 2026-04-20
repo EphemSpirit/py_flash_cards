10fbeeeb9f06f44688356ddbc7ce3498399e9514
Drew Hund <languageinvestigator@gmail.com>
2026-04-20 17:15:09 -0400

    auto flip

--- main.py
+++ main.py
@@ -6,20 +6,33 @@ BACKGROUND_COLOR = "#B1DDC6"
 
 data = pandas.read_csv("data/french_words.csv")
 cards = data.to_dict(orient="records")
+current_card = {}
 
 def show_card():
-    card = random.choice(cards)
+    global current_card
+    canvas.itemconfig(card_canvas_image, image=card_front_img)
+    current_card = random.choice(cards)
     canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=card["French"])
+    canvas.itemconfig(card_word, text=current_card["French"])
+    window.after(3000, flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_canvas_image, image=card_back_img)
 
 
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+window.after(3000, func=flip_card)
+
+
 canvas = Canvas(height=526, width=800)
 card_front_img = PhotoImage(file="./images/card_front.png")
-canvas.create_image(400, 263, image=card_front_img)
+card_canvas_image = canvas.create_image(400, 263, image=card_front_img)
+card_back_img = PhotoImage(file="images/card_back.png")
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="Answer", font=("Ariel", 60, "bold"))
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
@@ -34,6 +47,5 @@ known_button = Button(image=check_image, highlightthickness=0, command=show_card
 known_button.grid(row=1, column=1)
 
 show_card()
-
 
 window.mainloop()
