565588815223678e06cc35d1073d1704d4356a5f
Drew Hund <languageinvestigator@gmail.com>
2026-04-20 17:47:48 -0400

    saving to csv

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ import pandas
 import random
 
 BACKGROUND_COLOR = "#B1DDC6"
-
-data = pandas.read_csv("data/french_words.csv")
-cards = data.to_dict(orient="records")
 current_card = {}
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    data = pandas.read_csv("data/french_words.csv")
+    to_learn = data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+def is_known():
+    global current_card
+    to_learn.remove(current_card)
+    df = pandas.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+
+    show_card()
 
 def show_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
     canvas.itemconfig(card_canvas_image, image=card_front_img)
-    current_card = random.choice(cards)
+    current_card = random.choice(to_learn)
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=current_card["French"], fill="black")
     flip_timer = window.after(3000, func=flip_card)
@@ -44,7 +57,7 @@ unknown_button = Button(image=cross_img, highlightthickness=0, command=show_card
 unknown_button.grid(row=1, column=0)
 
 check_image = PhotoImage(file="./images/right.png")
-known_button = Button(image=check_image, highlightthickness=0, command=show_card)
+known_button = Button(image=check_image, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 show_card()
